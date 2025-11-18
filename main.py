134bf5d68f07601ff3b3e521d3602f2211e5339d
emanuellymattei <manuuu024@gmail.com>
2025-11-18 15:18:59 -0300

    feat: código tradução feito com a Sora Letícia

---
+++ main.py
@@ -0,0 +1,33 @@
+import html
+import requests
+
+def traduzir(text):
+    url = f"https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=pt-BR&q={html.unescape(text)}"
+    traducao = requests.get(url)
+
+    if traducao.status_code == 200:
+        traducao = traducao.json()
+        return traducao [0][0]
+    else:
+        print('Error fetching translator: status code: {traducao.status_code}')
+        return html.unescape(text)
+    
+#print(traduzir('The world is falling apart'))
+
+def buscarPerguntas():
+    url = f'https://opentdb.com/api.php?amount=6&category=10&difficulty=easy'
+    perguntas = requests.get(url)
+
+    if perguntas.status_code == 200:
+        perguntas = perguntas.json()
+        return perguntas['results']
+    else:
+        print(f'Error fetching Trivia: status code: {perguntas.status_code}')
+        return []
+    
+lista_de_perguntas = buscarPerguntas()
+
+for pergunta in lista_de_perguntas:
+    print(f'Original: {pergunta['question']}')
+    print(f'Tradução: {traduzir(pergunta['question'])}')
+    print('-------------------------------')
